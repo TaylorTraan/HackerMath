8e0b9df3574307c8254c20a56729b19d260f618f
Taylor Tran <taylortraan0709@gmail.com>
2024-11-09 13:35:37 -0800

    Initial Commit

---
+++ HackerMath/HackerMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerMathApp.swift
+//  HackerMath
+//
+//  Created by Taylor Tran on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct HackerMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
